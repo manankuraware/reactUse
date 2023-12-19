e17c730fce892ac1f2e266fe22e2a9d6a892cd82
manankuraware <manankuraware2017@gmail.com>
2023-12-19 17:13:12 +0530

    reactFormValidator

---
+++ formValidator/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Register from "./components/Register";
+function App() {
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+}
+export default App;
+/* first use Fontawesome :-
+npm i @fortawesome/fontawesome-svg-core
+npm i @fortawesome/free-solid-svg-icons
+npm i @fortawesome/react-fontawesome@latest
+*/
