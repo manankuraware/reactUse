c3b1e4dced3a314d27e3a6286b9794dc904a652b
manankuraware <manankuraware2017@gmail.com>
2024-01-09 21:53:15 +0530

    First version of application moved into GitHub

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -6,15 +6,27 @@ var logger = require('morgan');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const compression = require("compression");
+const helmet = require("helmet");
 const catalogRouter = require("./routes/catalog");
 
 
 var app = express();
 
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://mananrajeshkuraware:<#Cr7manan>@cluster0.oywramy.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://mananrajeshkuraware:<#Cr7manan>@cluster0.oywramy.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -25,10 +37,20 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
